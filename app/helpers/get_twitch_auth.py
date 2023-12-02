4347172f74d2c433a9ad3f5eeffa99046f5a1eb9
infinitescaling <scott.caling@gmail.com>
2023-12-02 16:48:21 -0500

    set up api to check for twitch

--- app/helpers/get_twitch_auth.py
+++ app/helpers/get_twitch_auth.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 ## This file will pull creds from a mounted drive
 
 def load_creds():
@@ -11,4 +12,25 @@ def load_creds():
     return {
         "client_id": client_id,
         "client_secret": client_secret
+    }
+
+def get_twitch_auth():
+    creds = load_creds()
+    client_id = creds["client_id"]
+    client_secret = creds["client_secret"]
+    
+    url = "https://id.twitch.tv/oauth2/token"
+    payload = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
+
+    response = requests.post(url, json = payload)
+    response_json = response.json()
+
+    access_token = response_json["access_token"]
+    return {
+        "access_token": access_token,
+        "client_id": client_id,
     }
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import requests
-from .helpers.get_twitch_auth import load_creds
+from .helpers.get_twitch_auth import get_twitch_auth
 app = FastAPI()
 
 
@@ -9,17 +9,27 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/live")
-async def check_live():
-    creds = load_creds()
-    client_id = creds["client_id"]
-    client_secret = creds["client_secret"]
+async def check_live(username: str):
+    twitch_auth = get_twitch_auth()
+    access_token = twitch_auth["access_token"]
+    client_id = twitch_auth["client_id"]
+    print(f"access_token: {access_token}")
+    print(f"client_id: {client_id}")
     
-    url = "https://id.twitch.tv/oauth2/token"
-    payload = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "grant_type": "client_credentials"
+    url = "https://api.twitch.tv/helix/streams?user_login=" + username
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Client-Id": client_id,
     }
-
-    x = requests.post(url, json = payload);
-
+    print(f"headers: {headers}")
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
+    print(response.status_code)
+    if response.status_code != 200  or not response_json["data"]:
+        return False
+    print(f"response_json: {response_json}")
+    # if response_json["data"]:
+    live_status = response_json["data"][0]["type"]
+    if live_status == "live":
+        return True
+    return "False"
