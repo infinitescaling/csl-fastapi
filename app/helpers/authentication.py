a05d6a211b81af1dbcc819fb3674e6ad61f77996
infinitescaling <scott.caling@gmail.com>
2023-12-04 20:59:20 -0500

    adding code to check env vars before deferring to local

--- app/helpers/authentication.py
+++ app/helpers/authentication.py
@@ -1,10 +1,17 @@
+import os
+
 def load_creds(cred_type: str):
 
-    with open(f"etc/{cred_type}/client_id/value") as f:
-        client_id = f.read()
+    try:
+        client_id = os.getenv('twitch_client_id')
+        client_secret = os.getenv('twitch_client_secret')
+    
+    except:
+        with open(f"etc/{cred_type}/client_id/value") as f:
+            client_id = f.read()
 
-    with open(f"etc/{cred_type}/client_secret/value") as f:
-        client_secret = f.read()
+        with open(f"etc/{cred_type}/client_secret/value") as f:
+            client_secret = f.read()
 
     return {
         "client_id": client_id,
